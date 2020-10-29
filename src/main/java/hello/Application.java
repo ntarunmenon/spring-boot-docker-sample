54b9f1a21911944211cb7f3b188c63303f4613fc
Arun Menon <arun.menon@autogeneral.com.au>
2020-10-30 07:44:45 +1000

    Adding unit test

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -4,13 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 @RestController
 public class Application {
 
+    private static Logger LOG = LoggerFactory.getLogger(Application.class);
+
     @RequestMapping("/")
     public String home() {
+        LOG.info("going to return hello world now");
         return "Hello Docker World";
     }
 
---
+++ src/test/java/hello/ApplicationTest.java
@@ -0,0 +1,13 @@
+package hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = Application.class)
+public class ApplicationTest {
+
+    @Test
+    public void testContextLoad() {
+
+    }
+}
